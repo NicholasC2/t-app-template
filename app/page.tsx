cb3c082eaaa2d7aa951b6bfe113a77ddbd74cdd5
NicholasC2 <137032388+NicholasC2@users.noreply.github.com>
2026-06-24 21:47:05 +0000

    updated to remove components replaced by @t-apps/ui, waiting for publish

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@
 
 import Image from "next/image";
 
-import Button from "../components/Button";
-import Input from "../components/Input";
-import Navbar from "../components/Navbar";
+// import Component from "@t-apps/ui"
 
 export default function Home() {
     return (
